f3b5e216684bdbc7c23e1b0a909e827dd905a006
橙子狂 <1141556380@qq.com>
2024-05-28 17:30:11 +0800

    0528
    简易完成 结构及fetch

---
+++ projects/4.歌词滚动案例/app/src/components/SongList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mediacontrol">
+        <audio controls="controls" src = "./music.mp3" ></audio>
+        <ul class="songlist">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const data = ref("")
+/*  */
+const fetchData = async () => {
+        const response = await fetch('http://project.x-zd.net:3001/apis/songword');
+        const result = await response.json();
+        data.value = result;
+        console.log(data.value,"data");
+    }
+
+onMounted(() =>{
+    fetchData();
+})
+</script>
+
+<style lang="scss" scoped>
+.mediacontrol{
+    width: 500px;
+    li{
+        list-style: none;
+    }
+}
+</style>
