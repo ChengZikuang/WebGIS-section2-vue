1cdab5451e327c744fbf5360d68f1cbfcf76b642
橙子狂 <1141556380@qq.com>
2024-06-13 17:04:09 +0800

    0613
    万年历创建

---
+++ projects/5.万年历/calendar/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+    async mounted(){
+        const response = await fetch("https://www.36jxs.com/api/Commonweal/almanac?sun=2024-06-14");
+        const res = await response.json();
+        console.log(res);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
